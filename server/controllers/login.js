55e0f03ba9d2c34293ef2f5a11b65da8cc848583
akhvesik <andrey.khvesik@vml.com>
2018-02-24 14:48:06 +0200

    code refactor controllers, create classes instead of functions

--- server/controllers/login.js
+++ server/controllers/login.js
@@ -1,65 +1,68 @@
 const UserModel = require('../models/user');
 const bcrypt = require('bcryptjs');
 
-const createNewUser = (req, res) => {
-  const {userName, password, role} = req.body;
-  const salt = bcrypt.genSaltSync(10);
-  const hashedPassword = bcrypt.hashSync(password, salt);
+class Login{
+  createNewUser(req, res) {
+    const {userName, password, role} = req.body;
+    const salt = bcrypt.genSaltSync(10);
+    const hashedPassword = bcrypt.hashSync(password, salt);
 
-  const newUser = new UserModel({
-    userName,
-    role,
-    password: hashedPassword
-  });
+    const newUser = new UserModel({
+      userName,
+      role,
+      password: hashedPassword
+    });
 
-  newUser.save()
-    .then(savedUser => {
-      res.json(savedUser)
-    })
-    .catch(err => {
-      res.send(err.message)
-    })
-};
-
-const authorize = (req, res) => {
-  const userDetails = req.body;
-
-  if (userDetails.userName && userDetails.password) {
-    UserModel.findOne({
-      userName: userDetails.userName
-    })
-      .then((user) => {
-        if (!user) {
-          res.json({
-            message: 'incorrect user name',
-            status: 204
-          });
-        } else {
-          if (bcrypt.compareSync(userDetails.password, user.password)) {
-            res.status(200).json({
-              id: user._id,
-              userName: user.userName,
-              role: user.role
-            });
-          } else {
-            res.json({
-              message: 'incorrect password'
-            });
-          }
-        }
+    newUser.save()
+      .then(savedUser => {
+        res.json(savedUser)
       })
       .catch(err => {
-        res.status(400).json(err);
+        res.send(err.message)
       })
-  } else {
-    res.status(400);
-    res.json({
-      message: 'wrong credentials'
-    })
-  }
-};
+  };
 
-module.exports = {
-  authorize,
-  createNewUser
-};
+  authorize(req, res) {
+    const userDetails = req.body;
+
+    if (userDetails.userName && userDetails.password) {
+      UserModel.findOne({
+        userName: userDetails.userName
+      })
+        .then((user) => {
+          if (!user) {
+            res.json({
+              message: 'incorrect user name',
+              status: 403
+            });
+          } else {
+            if (bcrypt.compareSync(userDetails.password, user.password)) {
+              res.json({
+                user: {
+                  id: user._id,
+                  userName: user.userName,
+                  role: user.role
+                },
+                status: 200
+              });
+            } else {
+              res.json({
+                message: 'incorrect password',
+                status: 403
+              });
+            }
+          }
+        })
+        .catch(err => {
+          res.status(400).json(err);
+        })
+    } else {
+      res.json({
+        message: 'wrong credentials',
+        status: 403
+      })
+    }
+  };
+}
+
+module.exports = new Login();
--- server/controllers/machine.js
+++ server/controllers/machine.js
@@ -1,35 +1,34 @@
 const MachineModel = require('../models/machine');
 
-const createNewMachine = (req, res) => {
-  const body = req.body;
+class Machine {
+  createNewMachine(req, res) {
+    const body = req.body;
 
-  const newMachine = new MachineModel(body);
+    const newMachine = new MachineModel(body);
 
-  newMachine.save()
-    .then(savedMachine => {
-      res.json({
-        status: 200,
-        message: 'success'
+    newMachine.save()
+      .then(savedMachine => {
+        res.json({
+          status: 200,
+          message: 'success'
+        })
       })
-    })
-    .catch(err => {
-      res.send(err.message)
-    })
-};
-
-const getAll = (req, res) => {
-  MachineModel.list()
-    .then(data => {
-      res.json(data)
-    })
-    .catch(err => {
-      res.status(400).json({
-        message: err.message
+      .catch(err => {
+        res.send(err.message)
       })
-    })
-};
+  };
 
-module.exports = {
-  getAll,
-  createNewMachine
-};
+  getAll(req, res) {
+    MachineModel.list()
+      .then(data => {
+        res.json(data)
+      })
+      .catch(err => {
+        res.status(400).json({
+          message: err.message
+        })
+      })
+  };
+}
+
+module.exports = new Machine();
