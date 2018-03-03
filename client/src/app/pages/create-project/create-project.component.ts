43ac76ec7327337d460d0feea74d6f67404eac92
akhvesik <andrey.khvesik@vml.com>
2018-03-03 13:53:01 +0200

    add reactive form to add project

--- client/src/app/pages/create-project/create-project.component.ts
+++ client/src/app/pages/create-project/create-project.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-create-project',
@@ -6,14 +8,99 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-project.component.scss']
 })
 export class CreateProjectComponent implements OnInit {
-  project: Object = {
-    title: '',
-    docs: ''
-  };
+  newTask() {
+    return new FormGroup({
+      name: new FormControl('',
+        [
+          Validators.required,
+          Validators.minLength(4),]
+      ),
+      description: new FormControl('',
+        [
+          Validators.required,
+          Validators.minLength(4),]
+      ),
+      machine: new FormControl('',
+        [
+          Validators.required,
+          Validators.minLength(4),])
+    });
+  }
 
-  constructor() { }
+  addTask(index) {
+    this.getTasks(index).push(this.newTask());
+  }
+
+  getTasks(index): FormArray {
+    return this.details.controls[index].get('tasks') as FormArray;
+  }
+
+  newDetail() {
+    return  new FormGroup({
+      name: new FormControl('',
+        [
+          Validators.required,
+          Validators.minLength(4),]
+      ),
+      description: new FormControl('',
+        [
+          Validators.required,
+          Validators.minLength(4),]
+      ),
+      tasks: new FormArray([
+        this.newTask()
+      ])
+    });
+  }
+
+  get details(): FormArray {
+    return this.createProjectForm.get('details') as FormArray;
+  }
+
+  addDetail() {
+    this.details.push(new FormGroup({
+        name: new FormControl('',
+          [
+            Validators.required,
+            Validators.minLength(4),]
+        ),
+        description: new FormControl('',
+          [
+            Validators.required,
+            Validators.minLength(4),]
+        ),
+        tasks: new FormArray([
+          this.newTask()
+        ])
+      })
+    );
+  }
+
+  createProjectForm = new FormGroup({
+    name: new FormControl('',
+      [
+        Validators.required,
+        Validators.minLength(4),]
+    ),
+    description: new FormControl('',
+      [
+        Validators.required,
+        Validators.minLength(4),]
+    ),
+    details: new FormArray([
+      this.newDetail()
+    ])
+  });
+
+  constructor() {
+  }
 
   ngOnInit() {
+  }
+
+  onCreateProject() {
+    console.log(this.createProjectForm)
+    console.log(this.createProjectForm.value)
   }
 
 }
